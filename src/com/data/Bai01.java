87a4f5e837315ecdf99b26aa4b44ee98dc7ec426
Fallon Knox <145274155+ThanhluanGif@users.noreply.github.com>
2025-06-08 17:49:16 +0700

    Add Program.java and Bai01.java - example of encapsulation

---
+++ src/com/data/Bai01.java
@@ -0,0 +1,31 @@
+package com.data;
+
+public class Bai01 {
+        private String name;
+        private int age;
+
+        // Getter cho name
+        public String getName() {
+            return name;
+        }
+
+        // Setter cho name
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        // Getter cho age
+        public int getAge() {
+            return age;
+        }
+
+        // Setter cho age
+        public void setAge(int age) {
+            if (age >= 0) { // kiểm tra điều kiện hợp lệ
+                this.age = age;
+            } else {
+                System.out.println("Tuổi không hợp lệ!");
+            }
+        }
+    }
+
---
+++ src/com/data/Program.java
@@ -0,0 +1,12 @@
+package com.data;
+
+public class Program {
+    public static void main(String[] args) {
+        Bai01 p = new Bai01();
+        p.setName("Luân");
+        p.setAge(22);
+
+        System.out.println(p.getName()); // Luân
+        System.out.println(p.getAge());  // 22
+    }
+}
